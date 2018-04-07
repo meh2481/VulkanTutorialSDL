bd6d08a55d65ef45be64d5a78925248f5a68ec11
meh2481 <meh2481@email.vccs.edu>
2018-04-07 15:18:24 -0400

    Image views

--- main.cpp
+++ main.cpp
@@ -97,6 +97,7 @@ private:
     std::vector<VkImage> swapChainImages;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
 
 public:
     //Public member functions
@@ -179,6 +180,35 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageView();
+    }
+
+    void createImageView()
+    {
+        swapChainImageViews.resize(swapChainImages.size());
+        for(size_t i = 0; i < swapChainImages.size(); i++)
+        {
+            VkImageViewCreateInfo createInfo = {};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if(vkCreateImageView(device, &createInfo, NULL, &swapChainImageViews[i]) != VK_SUCCESS)
+            {
+                std::cout << "failed to create image views" << std::endl;
+                exit(1);
+            }
+        }
     }
 
     void createSwapChain()
@@ -513,6 +543,8 @@ private:
 
     void cleanup()
     {
+        for(auto imageView : swapChainImageViews)
+            vkDestroyImageView(device, imageView, NULL);
         vkDestroySwapchainKHR(device, swapChain, NULL);
         vkDestroyDevice(device, NULL);
 #ifdef ENABLE_VALIDATION_LAYERS
